a014117516b84c9187e100dfd45fea93eaeb7786
sirenaarcheron <57989912+sirenaarcheron@users.noreply.github.com>
2019-11-23 10:42:27 -0300

    Add files via upload

---
+++ locadora.sql
@@ -0,0 +1,214 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 21-Nov-2019 às 20:08
+-- Versão do servidor: 10.4.8-MariaDB
+-- versão do PHP: 7.1.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `locadora`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `categoria`
+--
+
+CREATE TABLE `categoria` (
+  `idcategoria` int(11) NOT NULL,
+  `nome` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `classificacao`
+--
+
+CREATE TABLE `classificacao` (
+  `idclassificacao` int(11) NOT NULL,
+  `nome` varchar(20) NOT NULL,
+  `preco` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `cliente`
+--
+
+CREATE TABLE `cliente` (
+  `idcliente` int(11) NOT NULL,
+  `nome` varchar(100) NOT NULL,
+  `data_nasc` varchar(10) NOT NULL,
+  `rg` varchar(20) DEFAULT NULL,
+  `cpf` varchar(20) DEFAULT NULL,
+  `email` varchar(80) DEFAULT NULL,
+  `telefone` varchar(20) NOT NULL,
+  `bairro` varchar(45) NOT NULL,
+  `rua` varchar(45) NOT NULL,
+  `numero` int(11) NOT NULL,
+  `cep` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `dvd`
+--
+
+CREATE TABLE `dvd` (
+  `iddvd` int(11) NOT NULL,
+  `idfilme` int(11) NOT NULL,
+  `preco_compra` double DEFAULT NULL,
+  `data_compra` varchar(10) DEFAULT NULL,
+  `situacao` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `filme`
+--
+
+CREATE TABLE `filme` (
+  `idfilme` int(11) NOT NULL,
+  `titulo` varchar(100) NOT NULL,
+  `ano` int(11) DEFAULT NULL,
+  `duracao` varchar(10) DEFAULT NULL,
+  `idcategoria` int(11) NOT NULL,
+  `idclassificacao` int(11) NOT NULL,
+  `capa` varchar(80) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `funcionario`
+--
+
+CREATE TABLE `funcionario` (
+  `idfuncionario` int(11) NOT NULL,
+  `nome` varchar(100) NOT NULL,
+  `login` varchar(45) NOT NULL,
+  `senha` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `categoria`
+--
+ALTER TABLE `categoria`
+  ADD PRIMARY KEY (`idcategoria`);
+
+--
+-- Índices para tabela `classificacao`
+--
+ALTER TABLE `classificacao`
+  ADD PRIMARY KEY (`idclassificacao`);
+
+--
+-- Índices para tabela `cliente`
+--
+ALTER TABLE `cliente`
+  ADD PRIMARY KEY (`idcliente`);
+
+--
+-- Índices para tabela `dvd`
+--
+ALTER TABLE `dvd`
+  ADD PRIMARY KEY (`iddvd`),
+  ADD KEY `idfilme` (`idfilme`);
+
+--
+-- Índices para tabela `filme`
+--
+ALTER TABLE `filme`
+  ADD PRIMARY KEY (`idfilme`),
+  ADD KEY `idcategoria` (`idcategoria`),
+  ADD KEY `idclassificacao` (`idclassificacao`);
+
+--
+-- Índices para tabela `funcionario`
+--
+ALTER TABLE `funcionario`
+  ADD PRIMARY KEY (`idfuncionario`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `categoria`
+--
+ALTER TABLE `categoria`
+  MODIFY `idcategoria` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `classificacao`
+--
+ALTER TABLE `classificacao`
+  MODIFY `idclassificacao` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `cliente`
+--
+ALTER TABLE `cliente`
+  MODIFY `idcliente` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `dvd`
+--
+ALTER TABLE `dvd`
+  MODIFY `iddvd` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `filme`
+--
+ALTER TABLE `filme`
+  MODIFY `idfilme` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `funcionario`
+--
+ALTER TABLE `funcionario`
+  MODIFY `idfuncionario` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restrições para despejos de tabelas
+--
+
+--
+-- Limitadores para a tabela `dvd`
+--
+ALTER TABLE `dvd`
+  ADD CONSTRAINT `dvd_ibfk_1` FOREIGN KEY (`idfilme`) REFERENCES `filme` (`idfilme`);
+
+--
+-- Limitadores para a tabela `filme`
+--
+ALTER TABLE `filme`
+  ADD CONSTRAINT `filme_ibfk_1` FOREIGN KEY (`idcategoria`) REFERENCES `categoria` (`idcategoria`),
+  ADD CONSTRAINT `filme_ibfk_2` FOREIGN KEY (`idclassificacao`) REFERENCES `classificacao` (`idclassificacao`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
